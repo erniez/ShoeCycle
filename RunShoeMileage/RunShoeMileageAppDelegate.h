b8b46ce6c69ab1f87db40f366e1d32fe218f9467
Bob Bitchin <erniezappacosta@computer-3.home>
2011-12-03 18:35:52 -0600

    Initial Commit

---
+++ RunShoeMileage/RunShoeMileageAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RunShoeMileageAppDelegate.h
+//  RunShoeMileage
+//
+//  Created by Ernie on 12/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RunShoeMileageAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
