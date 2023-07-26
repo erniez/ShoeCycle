ccac85c9164c651662b0121f67528cfd8f6f5ed3
Ernie Zappacosta <ernest.zappacosta@gmail.com>
2023-07-25 21:01:04 -0700

    (Maintenance) Fix typography isses on the AddDistanceScreen. Limit dynamix font range.

---
+++ RunShoeMileage/DateDistanceEntryView.swift
@@ -0,0 +1,151 @@
+//  DateDistanceEntryView.swift
+//  ShoeCycle
+//
+//  Created by Ernie Zappacosta on 7/25/23.
+//  
+//
+
+import SwiftUI
+
+struct DateDistanceEntryView: View {
+    @State private var buttonMaxHeight: CGFloat?
+    @State private var showHistoryView = false
+    @State private var showFavoriteDistances = false
+    @State private var favoriteDistanceToAdd = 0.0
+    @Binding var runDate: Date
+    @Binding var runDistance: String
+    @ObservedObject var shoe: Shoe
+    @EnvironmentObject var shoeStore: ShoeStore
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Date:")
+                    .padding(.bottom, -4)
+                    .foregroundColor(.white)
+                
+                DatePicker("runDate",
+                           selection: $runDate,
+                           displayedComponents: [.date])
+                .accentColor(.shoeCycleOrange)
+                .foregroundColor(.white)
+                .labelsHidden()
+                .datePickerStyle(.compact)
+                .background(GeometryReader { geometry in
+                    Color.clear.preference(
+                        key: RowHeightPreferenceKey.self,
+                        value: geometry.size.height
+                    )
+                })
+                .frame(height: buttonMaxHeight)
+                .background(Color(uiColor: .systemGray6))
+                .cornerRadius(8)
+                
+                Button {
+                    showHistoryView = true
+                } label: {
+                    Label("History", systemImage: "calendar")
+                        .font(.callout)
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.shoeCycle)
+                .padding([.top], 4)
+                .fullScreenCover(isPresented: $showHistoryView) {
+                    let viewModel = HistoryListViewModel(shoeStore: shoeStore, shoe: shoe)
+                    HistoryListView(listData: viewModel)
+                }
+            }
+            .fixedSize()
+            .padding(.vertical, 8)
+            .padding(.leading, 8)
+            
+            VStack(alignment: .leading) {
+                Text("Distance:")
+                    .padding(.bottom, -4)
+                    .foregroundColor(.white)
+                
+                TextField(" Distance  ", text: $runDistance, prompt: Text(" Distance ").foregroundColor(Color(uiColor: .systemGray2)))
+                    .background(GeometryReader { geometry in
+                        Color.clear.preference(
+                            key: RowHeightPreferenceKey.self,
+                            value: geometry.size.height
+                        )
+                    })
+                    .frame(height: buttonMaxHeight)
+                    .frame(minWidth: 50)
+                    .textFieldStyle(.numberEntry)
+                    .toolbar {
+                        ToolbarItem(placement: .keyboard) {
+                            Button("Done") {
+                                dismissKeyboard()
+                            }
+                        }
+                    }
+                
+                Button {
+                    showFavoriteDistances = true
+                } label: {
+                    Label("Distances", systemImage: "heart.fill")
+                        .font(.callout)
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.shoeCycle)
+                .padding([.top], 4)
+                
+            }
+            .padding(.vertical, 8)
+            .padding(.leading, 8)
+            .fixedSize()
+            .fullScreenCover(isPresented: $showFavoriteDistances) {
+                FavoriteDistancesView(distanceToAdd: $favoriteDistanceToAdd)
+            }
+            .onChange(of: favoriteDistanceToAdd) { newValue in
+                if newValue > 0 {
+                    let formatter = NumberFormatter.decimal
+                    runDistance = formatter.string(from: NSNumber(value: favoriteDistanceToAdd)) ?? ""
+                }
+            }
+            
+            Spacer()
+            
+            Button {
+                dismissKeyboard()
+                guard let runDistanceNumber = Float(runDistance) else {
+                    print("Could not form a number from string entered")
+                    return
+                }
+                shoeStore.addHistory(to: shoe, date: runDate, distance: runDistanceNumber)
+                runDistance = ""
+                print(runDate)
+            } label: {
+                Image("button-add-run")
+            }
+            .padding(8)
+            .padding([.leading], 16)
+        }
+        .onPreferenceChange(RowHeightPreferenceKey.self) {
+            buttonMaxHeight = $0
+        }
+    }
+}
+
+private extension DateDistanceEntryView {
+    struct RowHeightPreferenceKey: PreferenceKey {
+        static let defaultValue: CGFloat = 0
+
+        static func reduce(value: inout CGFloat,
+                           nextValue: () -> CGFloat) {
+            value = max(value, nextValue())
+        }
+    }
+}
+
+struct DateDistanceEntryView_Previews: PreviewProvider {
+    @State static var runDate = Date()
+    @State static var runDistance = "5"
+    static var shoe = MockShoeGenerator().generateNewShoeWithData()
+    
+    static var previews: some View {
+        DateDistanceEntryView(runDate: $runDate, runDistance: $runDistance, shoe: shoe)
+    }
+}
