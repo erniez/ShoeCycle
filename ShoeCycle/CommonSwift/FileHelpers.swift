09af7fb7e88747a02db6ddba0f3473fbd1ed2274
Ernie Zappacosta <ernest.zappacosta@gmail.com>
2023-10-24 12:38:26 -0700

    (Maintenance)Remove LegacyCode group from the project structure. Fix where broken. Convert some straggling Objective-C code to Swift.

---
+++ ShoeCycle/CommonSwift/FileHelpers.swift
@@ -0,0 +1,8 @@
+//  FileHelpers.swift
+//  ShoeCycle
+//
+//  Created by Ernie Zappacosta on 10/24/23.
+//  
+//
+
+import Foundation
---
+++ ShoeCycle/CommonSwift/UIColorExtension.swift
@@ -0,0 +1,8 @@
+//  UIColorExtension.swift
+//  ShoeCycle
+//
+//  Created by Ernie Zappacosta on 10/24/23.
+//  
+//
+
+import Foundation
