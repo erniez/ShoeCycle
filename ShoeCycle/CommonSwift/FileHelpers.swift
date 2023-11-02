5e4ca35bd2d5ed639e8bdaae1947605a66419e19
Ernie Zappacosta <ernest.zappacosta@gmail.com>
2023-11-02 15:45:06 -0700

    (bugfix)Fix issue with formation of shoe image URL

--- ShoeCycle/CommonSwift/FileHelpers.swift
+++ ShoeCycle/CommonSwift/FileHelpers.swift
@@ -11,8 +11,10 @@ import Foundation
 enum FileHelpers {
     static func pathInDocumentDirectory(with filename: String) -> String? {
         let documentDirectories = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
-        if let documentDirectory = documentDirectories.first {
-            return documentDirectory + filename
+        if let documentDirectory = documentDirectories.first,
+            let documentsURL = URL(string: documentDirectory){
+            let fullURL = documentsURL.appendingPathComponent(filename)
+            return fullURL.absoluteString
         }
         return nil
     }
